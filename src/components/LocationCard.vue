b7d89ce7670df9db71d8d4ed66053688597784ab
Matt Bernhardt <mjbernha@mit.edu>
2020-05-04 21:38:32 -0400

    Adds stub LocationCard component

---
+++ src/components/LocationCard.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="location-card">
+    This is a location.
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,23 @@
 <template>
   <div class="home">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <LocationCard />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import LocationCard from '@/components/LocationCard.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    HelloWorld,
+    LocationCard
+  },
+  mounted () {
+    console.log('Home was mounted.')
   }
 }
 </script>
