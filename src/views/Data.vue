0fbce8221fedf4f7df3dc32bc623c3375cc5fe25
Matt Bernhardt <mjbernha@mit.edu>
2020-05-15 14:22:25 -0400

    Renames TermSummary to SheetSummary

--- src/views/Data.vue
+++ src/views/Data.vue
@@ -1,38 +1,34 @@
 <template>
   <div class="data">
     <div class="reference">This page has no reference to a current information display. It exists to surface the data from the API in a different, less processed, way.</div>
-    <TermSummary
-      v-for="(term, index) in terms"
+    <SheetSummary
+      v-for="(sheet, index) in store.state.data"
       :index="index"
-      :key="term.id"
-      :term="term" />
-    {{ terms }}
+      :key="sheet.id"
+      :sheet="sheet" />
+    {{ sheets }}
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import TermSummary from '@/components/TermSummary.vue'
+import SheetSummary from '@/components/SheetSummary.vue'
 
 export default {
-  name: 'Home',
+  name: 'Data',
   components: {
-    TermSummary
+    SheetSummary
   },
   data () {
     return {
       store: this.$root.$data,
-      terms: ''
+      sheets: ''
     }
   },
   computed: {
   },
   mounted () {
     this.store.setStatus('Mounting Data')
-    console.log('Home was mounted.')
-    this.axios
-      .get('https://spreadsheets.google.com/feeds/worksheets/1hK_4p-jx7dxW3RViRcBDSF_4En2QGgxx-Zy7zXkNIQg/public/basic?alt=json')
-      .then(response => (this.terms = response.data.feed.entry))
   }
 }
 </script>
